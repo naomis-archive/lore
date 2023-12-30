e33c4a89016d5ed7ed2343fe4510ac456247a211
Naomi Carrigan <nhcarrigan@gmail.com>
2023-12-29 19:45:12 -0800

    chore: :c

--- src/app/config/Avatars.ts
+++ src/app/config/Avatars.ts
@@ -1,6 +1,5 @@
 export const Avatars: Record<string, string> = {
   naomi: "https://cdn.nhcarrigan.com/avatars/naomi.png",
-  soup: "https://cdn.nhcarrigan.com/avatars/erin.png",
   melody: "https://cdn.nhcarrigan.com/avatars/melody.png",
   rosalia: "https://cdn.nhcarrigan.com/avatars/rosalia.png",
   becca: "https://cdn.nhcarrigan.com/avatars/becca.png"
